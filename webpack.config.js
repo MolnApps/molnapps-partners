58384eb78877c7bd34a9e5b7a7a0741aef217bfd
MolnApps <info@molnapps.com>
2020-09-16 01:44:03 +0300

    Extract styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
@@ -16,18 +17,18 @@ module.exports = {
 		minimize: true
 	},
 	module: {
-	    rules: [
-	      {
-	        test: /\.s[ac]ss$/i,
-	        use: [
-	          // Creates `style` nodes from JS strings
-	          'style-loader',
-	          // Translates CSS into CommonJS
-	          'css-loader',
-	          // Compiles Sass to CSS
-	          'sass-loader',
-	        ],
-	      },
-	    ],
-	  },
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+	],
 };
